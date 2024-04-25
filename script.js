141d24e5cbf160219aa3d610113f3d0408f2fd9f
0bobcode <167541110+0bobcode@users.noreply.github.com>
2024-04-25 13:27:42 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let myindex = 0;
+function carousel() {
+  let i;
+  let x = document.getElementsByClassName("carousel-item")
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myindex++;
+  if (myindex > x.length) {
+    myindex = 1
+
+  }
+  x[myindex - 1].style.display = "block";
+  setTimeout(carousel, 3000)
+}
+carousel();
+
